c6f89fd26056f947d7c8bbdd4b20333df65a5d61
Conner1324 <conner.shumney@yahoo.com>
2026-07-06 13:04:56 -0500

    Added private class

---
+++ StudentInfo/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    private String Firstname;
+    private String Lastname;
+    private int age;
+    private Double GPA;
+
+    public StudentType (String firstName, String lastName, int age, Double GPA) {
+        
+    }
+
+
+}
