56a610cd3ae90e3fb8e241e34ce3a50db7a193c9
Conner1324 <conner.shumney@yahoo.com>
2026-07-23 12:43:59 -0500

    Deleted all unrelated files in folder.

--- Assignment1.java
+++ Assignment1.java
@@ -1,18 +1,26 @@
 public class Assignment1 {
     public static void main(String[] args){
+        //Create array objects to hold inventory.
         String[] itemNames = new String [10];
-        Double[] itemPrices = new Double [10];
-        Integer[] itemQuantities = new Integer [10];
+        double[] itemPrices = new double [10];
+        int[] itemQuantities = new int [10];
 
-        // NOTE: I just added these items to confirm the program works. This will be removed once the inventory data is added.
-        itemNames[0] = "Bannana";
-        itemPrices[0] = 5.24;
-        itemQuantities[0] = 6;
+        // Fill the arrays with inventory.
+        String[] setNames= {"Banana", "Orange", "Milk", "Mango", "Grapes", "Pringles", "Coke", "Pepsi"};
+        double[] setPrices= {.99, 1.50, 3.25, 1.25, 1.50, 3.00, 1.25, 1.00};
+        int[] setQuantities= {15, 20, 5, 10, 20, 5, 40, 45};
+
+        // Initialize the arrays with the set values.
+        for (int i = 0 ; i < setNames.length; i++){
+            itemNames[i] = setNames[i];
+            itemPrices[i] = setPrices[i];
+            itemQuantities[i] = setQuantities[i];
+        }
 
         printInventory(itemNames, itemPrices, itemQuantities);
     }
 
-    public static void printInventory(String[] itemNames, Double[] itemPrices, Integer[] itemQuantities){
+    public static void printInventory(String[] itemNames, double[] itemPrices, int[] itemQuantities){
         for (int i = 0; i < itemNames.length; i++){
             if (itemNames[i] != null){
                 System.out.println(itemNames[i] + " - Price: $" + itemPrices[i] + " - Quantity: " + itemQuantities[i]);
--- Test.java
+++
@@ -1,6 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        System.out.println("Welcome to CS 3354 Object-Oriented Design and Programming");
-    }
-}
